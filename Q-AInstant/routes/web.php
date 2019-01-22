6132246ce5fdadccb6ce6c6fbee69f83a2e4e9fa
ROSSIER Quentin <Quentin.ROSSIER@cpnv.ch>
2019-01-22 10:28:58 +0100

    add new routes

--- Q-AInstant/routes/web.php
+++ Q-AInstant/routes/web.php
@@ -19,14 +19,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('addSurvey', function (){
-   return view('addSurvey');
-});
-
-Route::post('/addSurvey','SurveyController@create');
+Route::get('/addSurvey','SurveyController@createindex');
+Route::post('/addSurvey/create','SurveyController@store');
 
 Route::get('/teacher', 'SurveyController@index');
 
-Route::get('/resultSurvey/{id}','SurveyController@editindex');
+Route::get('/resultSurvey/{id}','SurveyController@results');
 
 
