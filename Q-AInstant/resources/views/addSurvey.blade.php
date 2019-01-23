7632181150cd5f61fab878c80ebdc3c42d08b49c
ROSSIER Quentin <Quentin.ROSSIER@cpnv.ch>
2019-01-23 18:55:05 +0100

    some new style

--- Q-AInstant/resources/views/addSurvey.blade.php
+++ Q-AInstant/resources/views/addSurvey.blade.php
@@ -25,15 +25,16 @@
                             </div>
                             <div class="card-header">Ajouter les questions</div>
                             <br>
-                            <div class="form-group row">
+                                <div class="form-group row">
 
-                                <label for="Q{{$Qn}}" class="col-md-4 col-form-label text-md-right">Question {{$Qn}}</label>
+                                    <label for="Q{{$Qn}}" class="col-md-4 col-form-label text-md-right">Question {{$Qn}}</label>
 
-                                <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="{{$Qn}}" required autofocus>
+                                    <div class="col-md-6">
+                                        <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="{{$Qn}}" required autofocus>
 
+                                    </div>
                                 </div>
-                            </div>
+
 
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
--- Q-AInstant/resources/views/layouts/app.blade.php
+++ Q-AInstant/resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
@@ -31,48 +32,8 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        @auth
-                            <li class="nav-item">
-                                <a class="nav-link" href="addSurvey">Créer un questionnaire</a>
-                            </li>
-                        @endauth
-                    </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-
-                    </ul>
                 </div>
             </div>
         </nav>
