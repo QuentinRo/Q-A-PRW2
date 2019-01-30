fdbba839aaea97b56408f12fb12d6335e3ff68a9
ROSSIER Quentin <Quentin.ROSSIER@cpnv.ch>
2019-01-30 19:29:02 +0100

    adding if to manage error

--- Q-AInstant/resources/views/doSurvey.blade.php
+++ Q-AInstant/resources/views/doSurvey.blade.php
@@ -3,27 +3,35 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{$survey->name}}</div>
 
-                    <div class="card-body">
-                        <form method="POST" action="/answerSurvey">
-                            @csrf
-                            <div class="form-group row">
-                        @foreach($questions as $question)
+                    @if ($survey == null)
+                            <h1>Aucun questionnaire n'est ouvert</h1>
+                        @else
+                        <div class="card">
+                            <div class="card-header">{{$survey->name}}</div>
 
-                                    <label for="answer" class="col-md-4 col-form-label text-md-right">{{$question->entitled}}</label>
+                                <div class="card-body">
 
-                                <div class="col-md-6">
-                                    <input type="text" class="form-control" name="answer[{{$question->id}}]">
-                                </div>
-                            <br><br>
-                        @endforeach
+                                <form method="POST" action="/answerSurvey">
+                                    @csrf
+                                    <div class="form-group row">
+                                        @foreach($questions as $question)
+
+                                            <label for="answer" class="col-md-4 col-form-label text-md-right">{{$question->entitled}}</label>
+
+                                            <div class="col-md-6">
+                                                <input type="text" class="form-control" name="answer[{{$question->id}}]">
+                                            </div>
+                                            <br><br>
+                                        @endforeach
+                                    </div>
+                                    <button  type="submit" class="btn btn-primary col-md-2 offset-md-8">Soumettre</button>
+                                </form>
                             </div>
-                            <button  type="submit" class="btn btn-primary col-md-2 offset-md-8">Soumettre</button>
-                        </form>
-                    </div>
-                </div>
+                        </div>
+                        @endif
+
+
             </div>
         </div>
     </div>
