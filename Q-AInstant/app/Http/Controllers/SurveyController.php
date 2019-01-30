ca12a7d33dd66682a7ee83517cb488312d7358c3
ROSSIER Quentin <Quentin.ROSSIER@cpnv.ch>
2019-01-30 19:29:41 +0100

    adding commentary and some error management

--- Q-AInstant/app/Http/Controllers/SurveyController.php
+++ Q-AInstant/app/Http/Controllers/SurveyController.php
@@ -12,30 +12,32 @@ use PHPUnit\Framework\Constraint\Attribute;
 class SurveyController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Show all survey, with the count of answers and if it's open or not
      *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-
         $surveys = Survey::All();
-        foreach ($surveys as $survey)
+
+        if ($surveys->count() == 0)
         {
-            $questions = Question::where('survey_id', '=',$survey->id)->get();
-
-            foreach ($questions as $question)
+            return view('teacher')->with('surveys', $surveys);
+        }else
+        {
+            foreach ($surveys as $survey)
             {
-                $answers[$survey->id] = Answer::where('question_id', '=', $question->id)->count();
+                $questions = Question::where('survey_id', '=',$survey->id)->get();
+
+                foreach ($questions as $question)
+                {
+                    $answers[$survey->id] = Answer::where('question_id', '=', $question->id)->count();
+                }
             }
-           // pour chaque tableau, liste les questions
-
-                //pour chaque question, compte les réponses
-            // $Acompter = Answer::where('question_id', '=', )->count();
+            return view('teacher')->with('surveys', $surveys)->with('nbanswers', $answers);
         }
-
-        return view('teacher')->with('surveys', $surveys)->with('nbanswers', $answers);
     }
+
+    // Open the selected Survey
     public function open($id){
 
         $open = Survey::where('open', '1')->first();
@@ -50,60 +52,37 @@ class SurveyController extends Controller
         }
      }
 
+     // Close the selected Survey
     public function close($id){
 
-
         Survey::where('id', '=', $id)->first()->update(['open' => 0]);
-        //
+
         return redirect('/teacher');
-        //self::results($id);
-        //return view('')
+
     }
     /**
-     * Show the form for creating a new resource.
+     * Create a new survey
      *
-     * @return \Illuminate\Http\Response
      */
     public function create(Request $request)
     {
-
-        //Faire un tableau contenant toutes les questions et les parcourires avec un foreach ?
-        // ou je sais pas. faire mieux ? :)
-
-        // https://stackify.com/laravel-eloquent-tutorial/
         $survey = new Survey;
         $survey->name = $request->name;
-
-
-        //$survey->description = $request->description;
-
         $survey->save();
 
-        /* or try this ?
-        Survey::create(array(
-            'name' => $survey->name,
-            'description' => $survey->description,
-            'created_at' => time(),
-            'update_at' => time()
-        ));
-        */
         return redirect('addSurvey');
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Show the "AddSurvey" page
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
-
     public function createindex(){
         $QuestionNumber = 1;
         return view('addSurvey')->with('Qn', $QuestionNumber);
     }
 
-
-    // all the honor are given to "Jarodo Mexico, the best Bro"
+    // Store the new survey with all questions (10 max)
     public function store(Request $request)
     {
         //create a new survey and all his questions
@@ -122,46 +101,46 @@ class SurveyController extends Controller
 
         return redirect('/teacher');
 
-        // have all the question in a table
-
-
-        //foreach Qnumber stock la question dans le tableau -> store ?
-        // check les relations pour stocker les questions
-
-
-        //$survey->save();
-
-        //$surveys = Survey::all();
-      //return view('teacher')->with('surveys', $surveys);
     }
 
+    // Show the "Open" Survey, to answer
     public function show()
     {
         $survey = Survey::where('open','1')->first();
-        $questions = Question::where('survey_id', $survey->id)->get();
 
-        return view('doSurvey')->with('survey', $survey)->with('questions', $questions);
+        if ($survey == null)
+        {
+            return view ('doSurvey')->with('survey', $survey);
+        }else
+        {
+            $questions = Question::where('survey_id', $survey->id)->get();
+            return view('doSurvey')->with('survey', $survey)->with('questions', $questions);
+        }
     }
 
+    // Save all the answers of the current survey
     public function answer(Request $request)
     {
-
         foreach($request->answer as $key=>$question)
         {
-            // ajout des questions dans la BDD
             $answer = new Answer;
             $answer->question_id = $key;
-            $answer->answer = $question;
-            $answer->save();
+
+            if ($question == null)
+            {
+                continue;
+            }else
+            {
+                $answer->answer = $question;
+                $answer->save();
+            }
         }
 
         return redirect('/');
     }
 
-
+    //return the survey with all his questions and answers
     public function results($id){
-
-        //return the survey with all his questions and answers
 
         $surveys = Survey::where('id', "=", $id)->first();
         $questions = Question::where('survey_id', "=", $id)->get();
@@ -170,35 +149,7 @@ class SurveyController extends Controller
             $answers[$question->id] = Answer::where('question_id', '=', $question->id)->get();
         }
 
-
         return view ('resultSurvey')->with('surveys', $surveys)->with('questions', $questions)->with('answers', $answers);
     }
 
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
